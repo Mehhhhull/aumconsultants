1c934d1875a4b84c12c6822e4290766b4402533d
Mehul Kumar Singh <mehulkumarsingh.2004@gmail.com>
2025-10-18 00:52:37 +0530

    Updated

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -3,14 +3,16 @@ const Logo = () => {
     <div className="flex items-center space-x-3">
       {/* Logo image */}
       <img
-        src="/path-to-your-logo.png" // replace with your logo path
+        src="src\assets\logo.jpg" // replace with your logo path
         alt="AUM Consultants Logo"
-        className="w-10 h-10 object-contain"
+        className="w-10 h-12 object-contain"
       />
 
       {/* Company name */}
       <div className="font-heading font-bold text-xl text-black">
-        AUM Consultants
+        <span className="text-primary">AUM</span>
+        <span>{' '}</span>
+        <span className="text-accent">Consultants</span>
       </div>
     </div>
   );
